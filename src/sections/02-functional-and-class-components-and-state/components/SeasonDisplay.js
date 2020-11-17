7fcf89e83839476cf09a20c6937b7ab7b0f30f38
lukaszdutka <lukaszdutka2@gmail.com>
2020-11-17 22:12:54 +0100

    pretty things up

--- src/sections/02-functional-and-class-components-and-state/components/SeasonDisplay.js
+++ src/sections/02-functional-and-class-components-and-state/components/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: 'sun'
+  },
+  winter: {
+    text: 'Burr it is cold!',
+    iconName: 'snowflake'
+  }
+}
+
 class SeasonDisplay extends React.Component {
 
   // second also can be state initialization
@@ -10,10 +21,17 @@ class SeasonDisplay extends React.Component {
 
   render() {
     if (this.props.latitude && !this.props.errorMessage) {
+      const season = getSeason(this.props.latitude, new Date().getMonth());
+      const {text, iconName} = seasonConfig[season];
       return (
-        <div> Latitude: {this.props.latitude}</div>
+        <div>
+          <i className={`${iconName} icon`}/>
+          <h1>{text}</h1>
+          <i className={`${iconName} icon`}/>
+        </div>
       )
     }
+
     if (!this.props.latitude && this.props.errorMessage) {
       return (
         <div> Error: {this.props.errorMessage}</div>
@@ -23,6 +41,13 @@ class SeasonDisplay extends React.Component {
       <div>Loading</div>
     )
   }
+}
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  }
+  return lat > 0 ? 'winter' : 'summer';
 }
 
 export default SeasonDisplay;
