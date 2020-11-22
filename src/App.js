e5bedd905a245d96e75065f217202b7eefafa131
lukaszdutka <lukaszdutka2@gmail.com>
2020-11-22 18:25:39 +0100

    boilerplate for next section - user input^

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css';
 import Components from './sections/01-components/Components.js'
 import Weather from './sections/02-functional-and-class-components-and-state/Weather.js'
+import UserInput from './sections/03-user-input/UserInput.js'
 
 const App = () => {
   return (
     <div>
       <Components/>
       <Weather/>
+      <UserInput/>
     </div>
   );
 }
---
+++ src/sections/03-user-input/UserInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class UserInput extends React.Component {
+
+  render() {
+    return (
+      <div className={'user-input-wrapper'}>Render component</div>
+    );
+  }
+}
+
+export default UserInput;
