e70171c40a3cea0722a81827dd6fda080dcb9073
lukaszdutka <lukaszdutka2@gmail.com>
2020-12-09 16:02:11 +0100

    debouncing - for the first time!

--- src/sections/05-hooks/Search.js
+++ src/sections/05-hooks/Search.js
@@ -3,36 +3,34 @@ import axios from 'axios';
 
 const Search = () => {
   const [term, setTerm] = useState('stoicism');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   useEffect(() => {
-    const search = () => {
-      if (term) {
-        axios.get('https://en.wikipedia.org/w/api.php', {
-          params: {
-            action: 'query',
-            list: 'search',
-            origin: '*',
-            format: 'json',
-            srsearch: term
-          }
-        }).then(({data}) => {
-          setResults(data.query.search)
-        })
-      }
-    };
-
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(search, 500)
-
-      return () => {
-        clearTimeout(timeoutId)
-      }
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
+    return () => {
+      clearTimeout(timerId)
     }
-
   }, [term])
+
+  useEffect(() => {
+    const search = () => {
+      axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: debouncedTerm
+        }
+      }).then(({data}) => {
+        setResults(data.query.search)
+      })
+    };
+    search();
+  }, [debouncedTerm])
 
   const renderedResults = results.map((result) => {
     return (
